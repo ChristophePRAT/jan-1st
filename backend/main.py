6108ac4c93d9f3c0cab92254a8cbb56cab29d30a
Christophe Prat <christopheprat@icloud.com>
2026-01-17 12:01:25 +0100

    initial flask api

--- backend/main.py
+++ backend/main.py
@@ -2,23 +2,36 @@ from strands import Agent
 from strands.models.openai import OpenAIModel
 from strands_tools import calculator
 import os
+from flask import Flask, request, jsonify
 
-def main():
-    model = OpenAIModel(
-        client_args={
-            "api_key": os.getenv("FEATHERLESS_API_KEY"),
-            "base_url": "https://api.featherless.ai/v1"
-        },
-        model_id="moonshotai/Kimi-K2-Thinking",
-        params={
-            "max_tokens": 1000,
-            "temperature": 0.2,
+app = Flask(__name__)
+
+model = OpenAIModel(
+    client_args={
+        "api_key": os.getenv("FEATHERLESS_API_KEY"),
+        "base_url": "https://api.featherless.ai/v1"
+    },
+    model_id="moonshotai/Kimi-K2-Thinking",
+    params={
+        "max_tokens": 1000,
+        "temperature": 0.2,
         }
-    )
+)
 
-    agent = Agent(model=model, tools=[calculator])
-    response = agent("What is exp(-8) * log_10(13298)?")
-    print(response)
+@app.route("/", methods=["POST"])
+def index():
+    data = request.json
+    print(f"Data: {data}")
+    agent = Agent(
+        model=model, 
+        tools=[calculator], 
+        callback_handler=None
+    )
+    response = agent(data["message"])
+    print("="*50)
+    print(f"Response: {response.message}")
+    print("="*50)
+    return jsonify({"response": response.message})
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
