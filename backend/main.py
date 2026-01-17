f9c095273a52e030cac8ab209d908e091d97574c
Christophe Prat <christopheprat@icloud.com>
2026-01-17 17:09:59 +0100

    allow unsafe werkzeug

--- backend/main.py
+++ backend/main.py
@@ -81,4 +81,4 @@ def handle_message(data):
     print("="*50)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=3000)
+    socketio.run(app, host="0.0.0.0", port=3000, allow_unsafe_werkzeug=True)
